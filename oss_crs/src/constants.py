af60d1175c1b6de611a1866bb9549c727f17946f
Andrew Chin <achin34@gatech.edu>
2026-06-02 06:22:59 -0400

    chore: postgres docker and update skill
    Signed-off-by: Andrew Chin <achin34@gatech.edu>

--- oss_crs/src/constants.py
+++ oss_crs/src/constants.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: MIT
 # Container images used by the infrastructure sidecar stack.
 LITELLM_IMAGE = "ghcr.io/berriai/litellm-database@sha256:49f891966bfa01c05c4e7cf5eb01387b8b2da5943556056d2d0e07333dbcd7af"  # v1.86.1
-POSTGRES_IMAGE = "postgres@sha256:f7ce845ee6873dd84be93c9828fe0d1fab0f9707dc9ac569694657398b290bce"  # 18.4
+POSTGRES_IMAGE = "postgres@sha256:8ff36f3c66371cba71d20ceedccfc3de9669a68737607888c4ef0af93abe8e39"  # 18.4
 
 # Internal LiteLLM proxy URL exposed inside the Docker network.
 LITELLM_INTERNAL_URL = "http://litellm.oss-crs:4000"
