a3fecc9091d2a2b9ef1a96b09ecc492052ebc487
Andrew Chin <achin34@gatech.edu>
2026-09-15 10:17:52 -0400

    chore(deps): sync litellm pin to constants.py
    Signed-off-by: Andrew Chin <achin34@gatech.edu>

--- oss_crs/src/constants.py
+++ oss_crs/src/constants.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: MIT
 # Container images used by the infrastructure sidecar stack.
-LITELLM_IMAGE = "ghcr.io/berriai/litellm-database@sha256:6b65a0c9f75ae120616447597ef277225d1f840d26b61dd84e62c3f04b2c15e6"  # v1.99.0
+LITELLM_IMAGE = "ghcr.io/berriai/litellm-database@sha256:070df884f52600352a4aedda059180be141f3645c61c59b89b0ee35c17a4ea84"  # v1.101.0
 POSTGRES_IMAGE = "postgres@sha256:4ef4dbc939d61acea57712655ddb4b4ab27419c913f94cca0cd57cb3ea3c2280"  # 18.6
 
 # Stable local tags for the internal LiteLLM stack images. ``prepare`` pulls
